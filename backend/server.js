37865b68d8df68398ecd05b7c126e03ebafe8a61
barath <barathkb2002@gmail.com>
2025-05-05 15:10:01 +0530

    chnaged

--- backend/server.js
+++ backend/server.js
@@ -28,8 +28,7 @@ app.use("/api/cart", cartRoutes);
 app.use("/api/coupons", couponRoutes);
 app.use("/api/payments", paymentRoutes);
 app.use("/api/analytics", analyticsRoutes);
-
-if (process.env.NODE_ENV === "development") {
+if (process.env.NODE_ENV === "production") {
 	app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
 	app.get("*", (req, res) => {
